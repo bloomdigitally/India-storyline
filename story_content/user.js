9bb0f387616cb333e9ac9f36718e9c0de62f4071
bloom digital <bloom.digitally@gmail.com>
2021-03-14 10:56:18 +0530

    India

---
+++ story_content/user.js
@@ -0,0 +1,31 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5fnvUGaryav":
+        Script1();
+        break;
+      case "6nsuOTf9BKy":
+        Script2();
+        break;
+      case "632BByYuYgf":
+        Script3();
+        break;
+  }
+}
+
+function Script1()
+{
+  window.print();
+}
+
+function Script2()
+{
+  window.print();
+}
+
+function Script3()
+{
+  window.print();
+}
+
